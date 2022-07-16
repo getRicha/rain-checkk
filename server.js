3064f818f3adeacb1964582ef3ef7411749edfa4
getRicha <richasri2002@gmail.com>
2022-07-17 02:08:11 +0530

    added get requests

--- server.js
+++ server.js
@@ -14,6 +14,22 @@ app.use(express.json())
 app.get('/',(req,res) => {
     res.sendFile(__dirname+'/index.html')
 })
+
+app.get('/coords/:city',async (req,res) => {
+    const city = req.params.city
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`)
+    const data = await response.json()
+    res.json(data)
+})
+
+app.get('/city/:coords',async (req,res) => {
+    const coords = req.params.coords.split(',')
+    const api_url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${coords[0]}&lon=${coords[1]}&limit=1&appid=${API_KEY}`
+    const data = await fetch(api_url)
+    const jsonData = await data.json()
+    res.json(jsonData)
+})
+
 app.get('/weather/:latlon',async (req,res) => {
     const latlon = req.params.latlon.split(',')
     const api_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latlon[0]}&lon=${latlon[1]}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
@@ -21,6 +37,7 @@ app.get('/weather/:latlon',async (req,res) => {
     const jsonData = await data.json()
     res.json(jsonData)
 })
+
 app.get('*',(req,res) => {
     res.render('404.ejs',{
         error: '404',
